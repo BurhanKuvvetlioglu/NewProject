5427da79f4da7f90df23bea811edce3355736b5b
Burhan Kuvvetlioglu <bkuvvetlioglu@hotmail.com>
2022-12-12 20:47:40 +0300

    TestClass1.java file update

--- src/package1/TestClass1.java
+++ src/package1/TestClass1.java
@@ -3,5 +3,6 @@ package package1;
 public class TestClass1 {
     public static void main(String[] args) {
         System.out.println("Trying");
+        System.out.println("Second attempt");
     }
 }
