803225af296e180c3cbc2bc656ee640241d609fb
Burhan Kuvvetlioglu <bkuvvetlioglu@hotmail.com>
2022-12-12 20:49:31 +0300

    Trial 1

--- src/package1/TestClass1.java
+++ src/package1/TestClass1.java
@@ -4,5 +4,6 @@ public class TestClass1 {
     public static void main(String[] args) {
         System.out.println("Trying");
         System.out.println("Second attempt");
+        //Trial 1
     }
 }
